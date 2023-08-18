9bb91cbb2d3d2bc23e6a158c6ed7a0e8f21fd241
unkabogaton <anthonralph2@gmail.com>
2023-08-19 02:11:58 +0800

    fixed signin function

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import axios from "axios";
 
 export const useUsers = defineStore("users", {
   state: () => ({
@@ -6,29 +7,32 @@ export const useUsers = defineStore("users", {
   }),
   actions: {
     async signIn(username, password) {
-      try {
-        const signInUrl =
-          "https://netzwelt-devtest.azurewebsites.net/Account/SignIn";
-        const signInData = { username, password };
-
-        const response = await fetch(signInUrl, {
-          method: "POST",
+      const signInUrl = "/api/Account/SignIn";
+      const signInData = {
+        username,
+        password,
+      };
+      axios
+        .post(signInUrl, signInData, {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(signInData),
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            const responseData = response.data; // This is the response data from the server
+            this.user = responseData;
+            console.log(this.user.displayName);
+            // Handle the response data or redirect
+          } else {
+            // Handle sign-in error
+            console.error("Sign-in failed");
+          }
+        })
+        .catch((error) => {
+          // Handle network or other errors
+          console.error("Error signing in:", error);
         });
-
-        if (response.ok) {
-          const user = await response.json();
-          this.user = user;
-          console.log(this.user);
-        } else {
-          console.error("Login failed");
-        }
-      } catch (error) {
-        console.error("Error:", error);
-      }
     },
   },
 });
