4425266e1cf5e2380737fd08ca7a6eef43085367
unkabogaton <anthonralph2@gmail.com>
2023-08-19 02:31:53 +0800

    Created fetch function of territories

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,39 @@
 <template>
-  <v-card>This is home</v-card>
+  <v-container
+    ><v-card v-if="user">This is home {{ user.displayName }} </v-card>
+    {{ territories }}
+  </v-container>
 </template>
 
 <script>
-export default {};
+import { useUsers } from "@/stores/counter";
+const store = useUsers();
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      user: store.user,
+      territories: [],
+    };
+  },
+  async created() {
+    await axios
+      .get("api/Territories/All")
+      .then((response) => {
+        if (response.status === 200) {
+          const responseData = response.data; // This is the response data from the server
+          this.territories = responseData;
+        } else {
+          console.error("Fetch failed");
+        }
+      })
+      .catch((error) => {
+        // Handle network or other errors
+        console.error("Error fetching:", error);
+      });
+  },
+};
 </script>
 
 <style></style>
