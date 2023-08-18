fbe83561399ecd4c574748934ecd0574a53fdb8f
unkabogaton <anthonralph2@gmail.com>
2023-08-19 02:11:33 +0800

    Fixed CORS error by using proxy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,16 @@ import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://netzwelt-devtest.azurewebsites.net",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [vue(), vuetify()],
   resolve: {
     alias: {
